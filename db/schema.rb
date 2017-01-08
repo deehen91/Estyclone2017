ae1472fc707b00256ea64a29f3301f5dba20228b
Darryl <darryl.henry91@gmail.com>
2017-01-08 18:34:20 +0000

    Adding dropbox support to carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104192330) do
+ActiveRecord::Schema.define(version: 20170108024855) do
 
-  create_table "listings", force: :cascade do |t|
-    t.string   "name"
+  create_table "listings", force: true do |t|
+    t.string   "name",        limit: nil
     t.text     "description"
     t.decimal  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "image"
   end
 
 end
